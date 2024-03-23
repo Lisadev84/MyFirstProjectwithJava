c15ae262a2f574f919dccddf4252ea82be93c1bf
Lisadev84 <118628266+Lisadev84@users.noreply.github.com>
2024-03-23 13:16:13 +0100

    Delete Topics directory

--- Topics/Formatted output/Simple combination/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {        
-        String name = "galactic hunter";
-        String learning  = "java";
-        String combinedString = String.format("%s learning %s ", name, learning);
-        System.out.println(combinedString);
-
-    }
-}
